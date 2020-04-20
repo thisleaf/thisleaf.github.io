d98dc911154d818eb35a686f6b857b99d260a14e
thisleaf <50230801+thisleaf@users.noreply.github.com>
2020-04-20 17:52:15 +0900

    Update 強化うｐ期間の更新

--- js/flower_exp.js
+++ js/flower_exp.js
@@ -5,7 +5,7 @@ const SHOW_DEBUG_BUTTON = false;
 // 経験値アップ期間は自動で経験値倍率をセット
 // 更新がめんどうなやつ
 const special_exp_factor_begin = new Date("2019/08/26");
-const special_exp_factor_end = new Date("2020/03/23 15:00");
+const special_exp_factor_end = new Date("2020/04/27 15:00");
 const special_exp_factor = 1.1;
 
 
